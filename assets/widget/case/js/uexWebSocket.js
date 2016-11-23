dd3d8bb3e2f293ad35e7b3a94132aa0d5ffbf2f9
fred <fei.ye@3g2win.com>
2016-11-23 15:24:27 +0800

    更新自动化测试的case

--- assets/widget/case/js/uexWebSocket.js
+++ assets/widget/case/js/uexWebSocket.js
@@ -8,8 +8,12 @@ if (UNIT_TEST) {
     var uexWebSocketCase = {
         "open": function () {
             uexWebSocket.open("ws://192.168.1.69:8887");
+            UNIT_TEST.assert(true);
+            uexWebSocket.close();
         },
         "onConnect":function () {
+
+            uexWebSocket.open("ws://192.168.1.69:8887");
             uexWebSocket.onConnect = function() {
                 UNIT_TEST.log("--------> connected.")
                 UNIT_TEST.assert(true);
@@ -17,11 +21,14 @@ if (UNIT_TEST) {
         },
         "send":function () {
             uexWebSocket.send("test data");
+            UNIT_TEST.assert(true);
         },
         "onMessage": function() {
             UNIT_TEST.log("请用服务端向客户端发送一条消息");
+            uexWebSocket.send("test data");
             uexWebSocket.onMessage = function(data) {
                 UNIT_TEST.log("[receive data]" + data);
+                UNIT_TEST.assert(true);
             }
         },
         "close": function () {
